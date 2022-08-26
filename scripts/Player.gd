b2482e966d34d4c8ef7c8891ac1de4294b21b343
GTNT-Github <graemeiburtis@gmail.com>
2022-08-26 13:57:17 -0400

    26.8.22 Fix 8
    - Player can no longer move while textbox is open

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -4,7 +4,6 @@ var faceLeft: bool = true
 
 
 func _process(delta: float) -> void:
-
 	var velocity: Vector2 = getInput()
 	move_and_slide(velocity*speed)
 
@@ -12,22 +11,23 @@ func getInput() -> Vector2:
 	var animatedSprite = get_node("Sprite")
 	var velocity: Vector2 = Vector2.ZERO
 	var animation: String = "idle"
-	if Input.is_key_pressed(KEY_W):
-		velocity.y -= 1
-		animation = "walk"
-	if Input.is_key_pressed(KEY_S):
-		velocity.y += 1
-		animation = "walk"
-	if Input.is_key_pressed(KEY_D):
-		velocity.x += 1
-		animation = "walk"
-		faceLeft = false
-	if Input.is_key_pressed(KEY_A):
-		velocity.x -= 1
-		animation = "walk"
-		faceLeft = true
-	animatedSprite.flip_h = faceLeft
-	if animation != animatedSprite.animation:
-		animatedSprite.play(animation)
+	if !Dialogic.has_current_dialog_node():
+		if Input.is_action_pressed("ui_up"):
+			velocity.y -= 1
+			animation = "walk"
+		if Input.is_action_pressed("ui_down"):
+			velocity.y += 1
+			animation = "walk"
+		if Input.is_action_pressed("ui_right"):
+			velocity.x += 1
+			animation = "walk"
+			faceLeft = false
+		if Input.is_action_pressed("ui_left"):
+			velocity.x -= 1
+			animation = "walk"
+			faceLeft = true
 		animatedSprite.flip_h = faceLeft
+		if animation != animatedSprite.animation:
+			animatedSprite.play(animation)
+			animatedSprite.flip_h = faceLeft
 	return velocity
