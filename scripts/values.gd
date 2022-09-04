82149dccf59deadda58f23cca8bac7f9391927b2
GTNT-Github <graemeiburtis@gmail.com>
2022-09-03 20:20:48 -0400

    3.9.22
    Added fight UI

--- scripts/values.gd
+++ scripts/values.gd
@@ -16,3 +16,17 @@ var itemStats = {
 	"Shield" : 10,
 	"Shield2" : 10
 }
+
+var flickrValues = {
+	0: Vector2(1.54,1.54),
+	2: Vector2(1.28,1.28),
+	4: Vector2.ONE
+}
+
+var sounds = {
+	"fightStart":preload("res://assets/sound/fightStart.wav")
+}
+
+var music = {
+	"fightSong":preload("res://assets/music/fightSong.wav")
+}
