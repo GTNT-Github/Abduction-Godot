bb1d5d7bea0958a279b86da57f491c25b9abae24
GTNT-Github <graemeiburtis@gmail.com>
2022-09-20 15:48:48 -0400

    1

--- scripts/UI/FightUI.gd
+++ scripts/UI/FightUI.gd
@@ -35,11 +35,11 @@ func flicker() -> void:
 		if n%2 == 0:
 			$Flicker.visible = false
 			rect_scale = Values.flickrValues[n]
-			GeneralFunc.playSound("fightStart")
+#			GeneralFunc.playSound("fightStart")
 			if n == 4:
 				cameras.fightCamera(Values.currentEnemy)
 				playText(Values.currentEnemy+"Approach","approach")
-				GeneralFunc.playMusic("fightSong")
+#				GeneralFunc.playMusic("fightSong")
 		else:
 			$Flicker.visible = true
 
@@ -48,7 +48,7 @@ func _input(event: InputEvent) -> void:
 	
 	#Check if fight is in progress
 	if Values.currentEnemy:
-		
+		print(Values.selectedButton)
 		#Switch to Item button
 		if event.is_action_pressed("ui_left") && Values.fightState == "menu" && Values.selectedButton == "Act":
 			setButton("Act","Items","button")
@@ -61,19 +61,40 @@ func _input(event: InputEvent) -> void:
 			
 		#Button Selection
 		elif event.is_action_pressed("ui_select") && Values.fightState == "menu":
-			get_node(Values.fightButtons["Act"]).visible = false
-			get_node(Values.fightButtons["Items"]).visible = false
-			get_node(Values.fightButtons[Values.selectedButton+"Text"]).visible = true
 			GeneralFunc.playSound("select")
-			
+			prints(Values.selectedButton != "Items",InvFunctions.inventory[0] != "-----")
+			if Values.selectedButton != "Items" || InvFunctions.inventory[0] != "-----":
+				Values.fightState = "text"
+				get_node(Values.fightButtons["Act"]).visible = false
+				get_node(Values.fightButtons["Items"]).visible = false
+				get_node(Values.fightButtons[Values.selectedButton+"Text"]).visible = true
+				setButton(str(Values.selectedButton,1),str(Values.selectedButton,1),"text")
+		
 		#Go back to buttons
-		elif event.is_action_pressed("ui_cancel") && Values.fightState == "menu":
+		elif event.is_action_pressed("ui_cancel") && Values.fightState == "text":
 			setButton(Values.selectedButton,Values.selectedButton,"button")
 			get_node(Values.fightButtons["ActText"]).visible = false
 			get_node(Values.fightButtons["ItemsText"]).visible = false
 			get_node(Values.fightButtons["Act"]).visible = true
 			get_node(Values.fightButtons["Items"]).visible = true
 			GeneralFunc.playSound("select")
+			Values.fightState = "menu"
+			var selectedButton = Values.selectedText
+			selectedButton[selectedButton.length()-1] = str(1)
+			setButton(Values.selectedText,selectedButton,"text")
+		
+		elif event.is_action_pressed("ui_down") && Values.fightState == "text":
+			var selectedText = int(Values.selectedText[Values.selectedText.length()-1])
+			if selectedText < 4:
+				var selectedButton = Values.selectedText
+				selectedButton[selectedButton.length()-1] = str(selectedText+1)
+				setButton(Values.selectedText,selectedButton,"text")
+		elif event.is_action_pressed("ui_up") && Values.fightState == "text":
+			var selectedText = int(Values.selectedText[Values.selectedText.length()-1])
+			if selectedText > 1:
+				var selectedButton = Values.selectedText
+				selectedButton[selectedButton.length()-1] = str(selectedText-1)
+				setButton(Values.selectedText,selectedButton,"text")
 
 #Set selected button/text
 func setButton(selectedButton, newButton,mode):
@@ -81,7 +102,10 @@ func setButton(selectedButton, newButton,mode):
 		get_node(Values.fightButtons[selectedButton]).modulate = Color(1,1,1,0.5)
 		get_node(Values.fightButtons[newButton]).modulate = Color(1,1,1,1)
 		Values.selectedButton = newButton
-		
+	elif mode == "text":
+		get_node(Values.fightButtons[selectedButton]).modulate = Color(1,1,1,1)
+		get_node(Values.fightButtons[newButton]).modulate = Color(1,1,1,0.5)
+		Values.selectedText = newButton
 #Play Text
 func playText(text,type):
 	var dialouge = Dialogic.start(text)
--- scripts/functions/GeneralFunc.gd
+++ scripts/functions/GeneralFunc.gd
@@ -4,14 +4,14 @@ func checkUI():
 	return !Dialogic.has_current_dialog_node() && !Values.invOpen && Values.fightState == "attack"
 
 func playSound(sound):
-	var soundNode = AudioStreamPlayer2D.new()
+	var soundNode = AudioStreamPlayer.new()
 	soundNode.stream = Values.sounds[sound]
 	add_child(soundNode)
 	soundNode.play()
 
 func playMusic(song):
-	var musicNode = AudioStreamPlayer2D.new()
+	var musicNode = AudioStreamPlayer.new()
 	musicNode.stream = Values.music[song]
-	musicNode.volume_db = 15
+#	musicNode.volume_db = 3
 	add_child(musicNode)
 	musicNode.play()
